23531de8151878c0711b644646af608fc19ad327
Mritunjay Raj <mriraj72600@gmail.com>
2022-10-19 17:52:34 +0530

    Assignment.

---
+++ Palindrome.js
@@ -0,0 +1,13 @@
+
+
+
+
+let str = "madam";
+let bag = "";
+for (i=str.length-1;i=0;i++){
+    bag+=str[i];
+} if(bag==str){
+    console.log("Yes");
+}else{
+    console.log("No")
+}
---
+++ PrimeNo.js
@@ -0,0 +1,12 @@
+
+let num = 13;
+ let sum =0 ;
+ for(i=1; i<=num; i++){
+     if(num%i==0){
+         sum++
+     }
+ }if(sum==1){
+     console.log("Yes")
+ }else{
+     console.log("No")
+ }
